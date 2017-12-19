47aa774a696bf77e5553ea247f89ecc22b681d93
katez <katiezechar@yahoo.com>
2017-12-19 01:57:17 +0000

    Add 'edit' capability on User Area Page

--- Mindspace_Interview/newScript.js
+++ Mindspace_Interview/newScript.js
@@ -1,1 +1,56 @@
+$('#edit-group').click(function(evt) {
+  var editInput = $('.interest-input')
+  var itemList = $('.passion-item')
 
+  console.log(itemList, 'this is  we have')
+
+  if (editInput.attr('class').indexOf('show') >= 0) {
+    editInput.addClass('hide');
+    itemList.addClass('show');
+    editInput.removeClass('show');
+    itemList.removeClass('hide');
+  } else {
+    editInput.addClass('show');
+    itemList.addClass('hide');
+    editInput.removeClass('hide');
+    itemList.removeClass('show');
+  }
+  return false;
+});
+
+$('#edit-practice-group').click(function(evt) {
+  var editInput = $('.practice-input')
+  var itemList = $('.practice-item')
+
+  console.log(itemList, 'this is  we have')
+
+  if (editInput.attr('class').indexOf('show') >= 0) {
+    editInput.addClass('hide');
+    itemList.addClass('show');
+    editInput.removeClass('show');
+    itemList.removeClass('hide');
+  } else {
+    editInput.addClass('show');
+    itemList.addClass('hide');
+    editInput.removeClass('hide');
+    itemList.removeClass('show');
+  }
+  return false;
+});
+
+/*var editButton = document.getElementById('edit-group');
+var editInput = document.getElementsByClassName('interest-input')
+var itemList = document.getElementsByClassName('passion-item')
+
+editButton.addEventListener('click', function(event) {
+  console.log(editInput, 'testing')
+  if (editInput.className === 'show') {
+    editInput.className = 'hide';
+    itemList.className = 'show';
+  } else {
+    editInput.className = 'show';
+    itemList.className = 'hide';
+  }
+  return false;
+});
+*/
